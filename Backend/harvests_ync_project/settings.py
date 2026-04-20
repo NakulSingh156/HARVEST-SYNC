4c2071732db77b1ef4685ea437c4f3c0d1ce3332
Nakul Singh <nakulsingh0910@gmail.com>
2026-04-20 07:09:45 +0530

    chore: Secure settings with python-dotenv

--- Backend/harvests_ync_project/settings.py
+++ Backend/harvests_ync_project/settings.py
@@ -1,21 +1,26 @@
 # Django settings for harvests_ync_project project.
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load environment variables from .env
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&9(iac027auqh(l#%9q$%63c3c(iptm$&eezbsp8p459#p+q+j'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-&9(iac027auqh(l#%9q$%63c3c(iptm$&eezbsp8p459#p+q+j')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
